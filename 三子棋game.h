eced76c5a8a6b5c54994516ed86d74a62a736e6c
heishanyangaerta <49788901+heishanyangaerta@users.noreply.github.com>
2019-05-19 21:59:05 +0800

    Create 三子棋game.h

---
+++ 三子棋game.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define ROW 3
+#define COL 3
+void intit(char board[ROW][COL], int row, int col);
+void shezhiqipan(char bord[ROW][COL], int row, int col);
+void wanjiawan(char board[ROW][COL], int row, int col);
+void diannaozou(char board[ROW][COL], int row, int col);
+char panduan(char board[ROW][COL], int row, int col);
